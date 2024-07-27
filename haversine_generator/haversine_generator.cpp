599394b2487f7dfe49454281938623c41a448d02
Alexandru Cazacu <alexandru.cazacu.9889@gmail.com>
2024-07-27 18:44:55 +0200

    chore: finish haversine generator

--- haversine_generator/haversine_generator.cpp
+++ haversine_generator/haversine_generator.cpp
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 #include <math.h>
 
 typedef uint8_t u8;
@@ -28,12 +29,12 @@ static u64 random_u64(RandomSeries* series) {
     u64 c = series->c;
     u64 d = series->d;
     
-    u64 E = a - rotate_left(b, 27);
+    u64 e = a - rotate_left(b, 27);
     
     a = (b ^ rotate_left(c, 17));
     b = (c + d);
-    c = (d + E);
-    d = (E + a);
+    c = (d + e);
+    d = (e + a);
     
     series->a = a;
     series->b = b;
@@ -87,22 +88,22 @@ static double square(double l) {
     return result;
 }
 
-static double radiansFromDegrees(double Degrees) {
+static double radians_from_degrees(double Degrees) {
     double Result = 0.01745329251994329577 * Degrees;
     return Result;
 }
 
 // NOTE(alex): EarthRadius is generally expected to be 6372.8
-static double referenceHaversine(double x0, double y0, double x1, double y1, double earthRadius) {
+static double reference_haversine(double x0, double y0, double x1, double y1, double earthRadius) {
     double lat1 = y0;
     double lat2 = y1;
     double lon1 = x0;
     double lon2 = x1;
     
-    double dLat = radiansFromDegrees(lat2 - lat1);
-    double dLon = radiansFromDegrees(lon2 - lon1);
-    lat1 = radiansFromDegrees(lat1);
-    lat2 = radiansFromDegrees(lat2);
+    double dLat = radians_from_degrees(lat2 - lat1);
+    double dLon = radians_from_degrees(lon2 - lon1);
+    lat1 = radians_from_degrees(lat1);
+    lat2 = radians_from_degrees(lat2);
     
     double a = square(sin(dLat / 2.0)) + cos(lat1) * cos(lat2) * square(sin(dLon / 2));
     double c = 2.0 * asin(sqrt(a));
@@ -154,17 +155,25 @@ static FILE* file_open(u64 pairCount, const char* label, const char* extension) 
 // cluser -> randomized square clusers to avoid total sum. convergence
 int main(int argc, char** argv) {
     if (argc != 4) {
-        printf("Usage: %s [uniform/cluster] [seed] [num. of pairs to generate]\n", argv[0]);
+        fprintf(stderr, "Usage: %s [uniform/cluster] [seed] [num. of pairs to generate]\n", argv[0]);
         return 1;
     }
     
+    u64 clusterCountLeft = U64Max;
+    
     char* methodName = argv[1];
+    if (strcmp(methodName, "cluster") == 0) {
+        clusterCountLeft = 0;
+    } else if (strcmp(methodName, "uniform") != 0) {
+        methodName = "uniform";
+        fprintf(stderr, "WARNING: Unrecognized method name. Using 'uniform'.\n");
+    }
     
     u64 maxPairCount = (1ULL << 34);
     u64 pairCount = atoll(argv[3]);
     
     if (pairCount >= maxPairCount) {
-        printf("To avoid accidentally generating massive files, number of pairs must be less than %llu.\n", maxPairCount);
+        fprintf(stderr, "To avoid accidentally generating massive files, number of pairs must be less than %llu.\n", maxPairCount);
         return 1;
     }
     
@@ -182,10 +191,46 @@ int main(int argc, char** argv) {
     double xRadius = maxAllowedX;
     double yRadius = maxAllowedY;
     
+    u64 clusterCountMax = 1 + (pairCount / 64);
+    
     if (json && binaryAnswers) {
+        double sum = 0;
+        double sumCoeff = 1.0 / (double)pairCount;
+        
+        fprintf(json, "{\"pairs\":[\n");
+        
         for (u64 i = 0; i < pairCount; ++i) {
+            if(clusterCountLeft-- == 0) {
+                clusterCountLeft = clusterCountMax;
+                xCenter = random_in_range(&series, -maxAllowedX, maxAllowedX);
+                yCenter = random_in_range(&series, -maxAllowedY, maxAllowedY);
+                xRadius = random_in_range(&series, 0, maxAllowedX);
+                yRadius = random_in_range(&series, 0, maxAllowedY);
+            }
+            
             double x0 = random_degree(&series, xCenter, xRadius, maxAllowedX);
+            double y0 = random_degree(&series, yCenter, yRadius, maxAllowedY);
+            double x1 = random_degree(&series, xCenter, xRadius, maxAllowedX);
+            double y1 = random_degree(&series, yCenter, yRadius, maxAllowedY);
+            
+            double earthRadius = 6372.8;
+            double haverDistance = reference_haversine(x0, y0, x1, y1, earthRadius);
+            
+            sum += sumCoeff * haverDistance;
+            
+            const char* jsonSep = (i == (pairCount - 1)) ? "\n" : ",\n";
+            fprintf(json, "    {\"x0\":%.16f, \"y0\":%.16f, \"x1\":%.16f, \"y1\":%.16f}%s", x0, y0, x1, y1, jsonSep);
+            
+            fwrite(&haverDistance, sizeof(haverDistance), 1, binaryAnswers);
         }
+        
+        fprintf(json, "]}\n");
+        fwrite(&sum, sizeof(sum), 1, binaryAnswers);
+        
+        fprintf(stdout, "Method: %s\n", methodName);
+        fprintf(stdout, "Random seed: %llu\n", seedValue);
+        fprintf(stdout, "Pair count: %llu\n", pairCount);
+        fprintf(stdout, "Expected sum: %.16f\n", sum);
     }
     
     if (json) {
@@ -194,7 +239,6 @@ int main(int argc, char** argv) {
     if (binaryAnswers) {
         fclose(binaryAnswers);
     }
-    
     
     return 0;
 }
